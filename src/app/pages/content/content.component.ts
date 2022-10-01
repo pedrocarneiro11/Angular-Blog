d02f6b0eb72766778ce1c966a03010b606008356
Pedro <pedrocarneirocunha87@gmail.comgit config --global user.email pedrocarneirocunha87@gmail.comgit config --global user.email pedrocarneirocunha87@gmail.com>
2022-10-01 10:24:06 -0300

    commit

--- src/app/pages/content/content.component.ts
+++ src/app/pages/content/content.component.ts
@@ -6,6 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./content.component.css']
 })
 export class ContentComponent implements OnInit {
+  photoCover:string="https://static3.cbrimages.com/wordpress/wp-content/uploads/2020/06/tony-stark-endgame-snap.jpg"
+  contentTitle:string="MEU ARTIGO"
+  description:string="MEU PARAGRAFO"
 
   constructor() { }
 
